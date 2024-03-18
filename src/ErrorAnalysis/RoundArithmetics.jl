e9798f5a6e6de77b53df2cb2244585755a920eb8
Vladenidis <163321151+Vladenidis@users.noreply.github.com>
2024-03-18 10:38:43 -0300

    Add files via upload

---
+++ src/ErrorAnalysis/RoundArithmetics.jl
@@ -0,0 +1,33 @@
+function round_sum(x::Float64, y::Float64; digits::Int64)
+    """
+    Implement a function that round x and y, then sum both.
+    After, round again.
+
+    hint: use "round" function.
+    """
+end
+
+function round_mul(x::Float64, y::Float64; digits::Int64)
+    """
+    Implement a function that round x and y, then multiply both.
+    After, round again.
+    """
+end
+
+function trunc_sum(x::Float64, y::Float64; digits::Int64)
+    """
+    Implement a function that trunc (chopp) x and y, then sum both.
+    After, chopp again.
+
+    hint: use "trunc" function.
+    """
+end
+
+function trunc_mul(x::Float64, y::Float64; digits::Int64)
+    """
+    Implement a function that trunc (chopp) x and y, then multiply both.
+    After, chopp again.
+
+    hint: use "trunc" function.
+    """
+end
